f8e61319b22688a655f2ec5b5641380a5c5e7eb2
Eric Stiens <estiens@users.noreply.github.com>
2022-02-10 16:22:41 -0600

    backend initial commit

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'json'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+before do
+  headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Headers'] = 'Accept, Authorization, Origin'
+end
+
+options '*' do
+  response.headers['Allow'] = 'HEAD, GET, PUT, DELETE, OPTIONS, POST'
+  response.headers['Access-Control-Allow-Headers'] =
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept'
+end
+
+get '/' do
+  { message: 'hello', meta: ENV['SPECIAL_META'] }.to_json
+end
